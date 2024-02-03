47a21ea826708f6031d91ab097df3c4332f294a3
pooja <eng21ct0029@dsu.edu.in>
2024-02-03 20:51:32 +0530

    initial commit

--- app/page.js
+++ app/page.js
@@ -1,113 +1,202 @@
-import Image from "next/image";
+"use client";
+import React, {
+  useState,
+  useEffect
+} from "react";
+import {
+  ToastContainer,
+  toast
+} from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
-export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+function CaretakersPage() {
+  const [caretakersData, setCaretakersData] = useState([]);
+  const [nurseryIds, setNurseryIds] = useState([]);
+  const [selectedNurseryId, setSelectedNurseryId] = useState("All");
+  const [selectedCaretaker, setSelectedCaretaker] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const [errorShown, setErrorShown] = useState(false);
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+  useEffect(() => {
+    fetchNurseryIds();
+    fetchCaretakersData();
+  }, []);
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+  useEffect(() => {
+    setErrorShown(false); // Reset errorShown when selectedNurseryId changes
+  }, [selectedNurseryId]);
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+  const fetchNurseryIds = () => {
+    fetch("http://localhost:8080/nurseries")
+      .then((response) => response.json())
+      .then((data) => {
+        const ids = data.map((nursery) => nursery.nurseryId);
+        setNurseryIds(ids);
+      })
+      .catch((error) => {
+        console.error("Error fetching nursery IDs:", error);
+      });
+  };
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
+  const fetchCaretakersData = () => {
+    fetch("http://localhost:8080/caretakers")
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return response.json();
+      })
+      .then((data) => {
+        setCaretakersData(data);
+        setIsLoading(false);
+      })
+      .catch((error) => {
+        console.error("Error fetching caretakers:", error);
+        if (!errorShown) {
+          toast.error("Failed to fetch caretakers data.");
+          setErrorShown(true);
+        }
+      });
+  };
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  );
+  const handleNurserySelect = (event) => {
+    setSelectedNurseryId(event.target.value);
+    setErrorShown(false); // Reset errorShown when a new nursery is selected
+  };
+
+  const displayCaretakersByNursery = (nurseryId) => {
+    if (isLoading) {
+      return [];
+    }
+
+    let filteredCaretakers = [];
+    if (nurseryId === "All") {
+      filteredCaretakers = caretakersData;
+    } else {
+      filteredCaretakers = caretakersData.filter(
+        (caretaker) => caretaker.nursery.nurseryId === nurseryId
+      );
+    }
+
+    // Check if there are no caretakers and the error hasn't been shown yet
+    if (filteredCaretakers.length === 0 && !errorShown) {
+      toast.error(`No caretakers found for nursery ID: ${nurseryId}`);
+      setErrorShown(true); // Set errorShown to true to prevent showing the error again
+    }
+
+    return filteredCaretakers;
+  };
+
+
+  const handleCaretakerClick = (caretaker) => {
+    setSelectedCaretaker(caretaker);
+  };
+
+  const handleClosePopup = () => {
+    setSelectedCaretaker(null);
+  };
+
+  return ( <
+      div className = "caretakers-page-container" >
+      <
+      ToastContainer / >
+      <
+      div className = "dropdown-container" >
+      <
+      select className = "nursery-dropdown"
+      value = {
+        selectedNurseryId
+      }
+      onChange = {
+        handleNurserySelect
+      } >
+      <
+      option value = "All" > All < /option> {
+      nurseryIds.map((nurseryId) => ( <
+        option key = {
+          nurseryId
+        }
+        value = {
+          nurseryId
+        } > {
+          nurseryId
+        } < /option>
+      ))
+    } <
+    /select> < /
+    div > <
+    div className = "caretakers-grid" > {
+      displayCaretakersByNursery(selectedNurseryId).map((caretaker) => ( <
+        div key = {
+          caretaker.caretakerId
+        }
+        className = "caretaker-container"
+        onClick = {
+          () => handleCaretakerClick(caretaker)
+        } >
+        <
+        p > Caretaker ID: {
+          caretaker.caretakerId
+        } < /p> <
+        p > Name: {
+          caretaker.name
+        } < /p> < /
+        div >
+      ))
+    } <
+    /div> {
+  selectedCaretaker && ( <
+    >
+    <
+    div className = "backdrop"
+    onClick = {
+      handleClosePopup
+    } > < /div> <
+    div className = "popup-container" >
+    <
+    div className = "popup-content" >
+    <
+    span className = "close-button"
+    onClick = {
+      handleClosePopup
+    } > X < /span> <
+    p > Caretaker ID: {
+      selectedCaretaker.caretakerId
+    } < /p> <
+    p > Name: {
+      selectedCaretaker.name
+    } < /p> <
+    p > Age: {
+      selectedCaretaker.age
+    } < /p> <
+    p > Phone Number: {
+      selectedCaretaker.phoneNumber
+    } < /p> <
+    p > Email: {
+      selectedCaretaker.email
+    } < /p> <
+    p > Sex: {
+      selectedCaretaker.sex
+    } < /p> <
+    p > Years of Experience: {
+      selectedCaretaker.yearsOfExperience
+    } < /p> <
+    p > Designation: {
+      selectedCaretaker.designation
+    } < /p> <
+    p > Average Rating: {
+      selectedCaretaker.averageRating
+    } < /p> <
+    p > Nursery ID: {
+      selectedCaretaker.nursery.nurseryId
+    } < /p> < /
+    div > <
+    /div> < /
+    >
+  )
+} <
+/div>
+);
 }
+
+export default CaretakersPage;
